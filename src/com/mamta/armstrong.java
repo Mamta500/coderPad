b70977f1062d0b793287635c0ae9e608d2bfaa30
Mamta Mulchandani <mmulchan@in.ibm.com>
2022-03-11 23:55:09 +0530

    armstrong

---
+++ src/com/mamta/armstrong.java
@@ -0,0 +1,13 @@
+package com.mamta;
+
+public class armstrong {
+
+    public static boolean armstrong(int num){
+        while(num>0){
+            int a = num%10;
+
+
+        }
+        return true;
+    }
+}
